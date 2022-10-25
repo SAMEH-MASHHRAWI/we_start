3ff8775d76a4131f975440f45abef7919a5f8f44
sameh mashhrawi <92118065+SAMEH-MASHHRAWI@users.noreply.github.com>
2022-10-26 01:51:37 +0300

    lec15-product-filter

---
+++ lec15-vue-product-filter/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <RouterLink class="logo" to="/">Products Filter</RouterLink>
+      <nav>
+        <RouterLink  to="/">Home</RouterLink>
+        <RouterLink  to="/products">Products</RouterLink>
+      </nav>
+    </header>
+    <RouterView />
+  </div>
+</template>
---
+++ lec15-vue-product-filter/src/views/Products.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <section class="main-wrapper">
+      <div class="side">
+        side
+        <h3>Categories</h3>
+        <label><input type="checkbox" name="" />sport</label>
+        <label><input type="checkbox" name="" />sport</label>
+        <label><input type="checkbox" name="" />sport</label>
+        <label><input type="checkbox" name="" />sport</label>
+        <hr />
+        <h3>Colors</h3>
+        <label><input type="checkbox" name="" />red</label>
+        <label><input type="checkbox" name="" />blue</label>
+        <label><input type="checkbox" name="" />green</label>
+        <label><input type="checkbox" name="" />sport</label>
+        <hr />
+        <h3>price</h3>
+        Min: <br />
+        <input type="range" />
+        <br />
+        Mix: <br />
+        <input type="range" />
+      </div>
+      <div class="content">
+        <product-item v-for="product in products" :key="product.id" />
+         <ProductItem />
+       
+      </div>
+    </section>
+  </div>
+</template>
+<style scoped>
+.main-wrapper {
+  display: flex;
+  margin: 60px 0;
+}
+
+.main-wrapper .side {
+  width: 30%;
+  padding: 0 24px;
+}
+.main-wrapper .content {
+  width: 70%;
+  padding: 0 24px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+}
+.side label {
+  display: block;
+}
+</style>
+
+<script>
+import ProductItem from "../components/ProductItem.vue";
+import products from '../data/products.json' 
+export default {
+  components: { ProductItem },
+  data() {
+    return {
+      products
+    };
+  },
+};
+</script>
