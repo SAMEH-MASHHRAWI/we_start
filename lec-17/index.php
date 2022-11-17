2b8e5f51fa00313366e7920508a64b2afab8956b
sameh mashhrawi <92118065+SAMEH-MASHHRAWI@users.noreply.github.com>
2022-11-17 23:13:15 +0200

    lec17

---
+++ lec-17/index.php
@@ -0,0 +1,58 @@
+<?php
+//$family = [
+//    'sameh' ,'mohammed','karam','rawia'];
+//echo '<pre>';
+//var_dump($family);
+//'</pre>';
+
+
+//$family =[
+//    ['sameh','aya','omar'],
+//    ['rawia','sameh','hoseen'],
+//    ['eslam','kareem','yaseen'],
+//];
+//var_dump($family);
+
+//$ages=[20,52,25,30,10,18,25,23,22,21,19];
+//
+//$ages = [
+//    'sameh' => 20,
+//    'hoseen' => 27,
+//    'rawia' => 55,
+//    'walaa' => 28,
+//    'alaa' => 28,
+//    'eslam' => 31,
+//];
+//$ages['malak']=22;
+//var_dump($ages);
+//echo $ages['sameh'];
+
+
+$family =[
+    'eslam','mohammed','mahmoud','walaa','alaa','hoseen','sameh','aya','ali'
+];
+
+
+
+// for($s=0;$s<7;$s++){
+//     echo $family[$s].'<br>';
+// } 
+
+// $s=0;
+// while($s <count ($family)){
+//     echo $family[$s].'<br>'.'While';
+//     $s++;
+// }
+
+// $s=0;
+// do{
+//     echo $family[$s].'<br>'.'Do...While';
+//     $s++;
+// }while($s <count ($family));
+
+
+
+// infinte loop
+// for ($i=0 ; true ; $i++){
+//     echo $i;
+// }
